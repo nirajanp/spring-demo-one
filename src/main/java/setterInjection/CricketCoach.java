6dc67f487900722898ab27b9f215eb27cbc65619
Nirajan Pandey <nirajanp@Nirajans-MBP.fios-router.home>
2021-09-22 00:57:49 -0400

    minor changes in setterInjection

--- src/main/java/setterInjection/CricketCoach.java
+++ src/main/java/setterInjection/CricketCoach.java
@@ -3,7 +3,11 @@ package setterInjection;
 public class CricketCoach implements Coach{
     private FortuneService fortuneService;
 
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside no-arg constructor");
+    }
     public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach. inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
     }
 
--- src/main/java/setterInjection/MyApp.java
+++ src/main/java/setterInjection/MyApp.java
@@ -4,9 +4,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
     public static void main(String[] args) {
+        // load spring config file
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
         Coach theCoach = context.getBean("theCricketCoach", Coach.class);
+
+        // call methods on the bean
+        System.out.println(theCoach.getYourDailyWorkout());
         System.out.println(theCoach.getYouDailyFortune());
+
+        //close the context
+        context.close();
     }
 }
